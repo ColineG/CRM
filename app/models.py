a9cd30c1946bab06e21339b315e3a0e36c273e76
Coline Gilles <colinegilles@gmail.com>
2019-11-21 14:20:58 +0100

    creation class amount, invoice, quote

--- app/models.py
+++ app/models.py
@@ -1,30 +1,32 @@
 from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy import Column, String, Integer
-from sqlalchemy.types import DateTime
+from sqlalchemy.types import DateTime, Numeric
 from sqlalchemy.dialects.postgresql import VARCHAR, ENUM
 from sqlalchemy_utils import EmailType
 from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import Config, config, FlaskConfig
-from app import db
+from vcf_extractor import  CustomerExtractor
 
 # create an engine
 conf = FlaskConfig()
 db = create_engine(conf.DATABASE_URL)
 base = declarative_base()
 
-# configuration
+# configuration to file db
+conf = Config(config)
+x = CustomerExtractor(conf.clients)
 
 
 class Email(base):
     __tablename__ = 'email'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    id_contact = Column(Integer, ForeignKey('contact.id'))
+    contact_id = Column(Integer, ForeignKey('contact.id'))
     email = Column(EmailType, unique=True)
     label = Column(String(100))
-    id_company = Column(Integer, ForeignKey('company.id'))
+    company_id = Column(Integer, ForeignKey('company.id'))
 
     def __repr__(self):
         return '<Email {}>'.format(self.email)
@@ -34,7 +36,7 @@ class Fn(base):
     __tablename__ = 'company'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    id_contact = Column(Integer, ForeignKey('contact.id'))
+    contact_id = Column(Integer, ForeignKey('contact.id'))
     company_name = Column(String(100), unique=True)
 
     def __repr__(self):
@@ -48,8 +50,8 @@ class N(base):
     last_name = Column(String(255))
     first_name = Column(String(255))
     prefix = Column(String(255))
-    id_company = Column(Integer, ForeignKey('company.id'))
-    contact_status = Column(ENUM('Client', 'Prospect', 'Churn'))
+    company_id = Column(Integer, ForeignKey('company.id'))
+    contact_status = Column(ENUM('Client', 'Prospect', 'Churn', 'Prestataire', name='contact_status'))
 
     def __repr__(self):
         return '<N {}>'.format(self.last_name)
@@ -59,8 +61,8 @@ class Note(base):
     __tablename__ = 'follow_up'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    id_contact = Column(Integer, ForeignKey('contact.id'))
-    id_company = Column(Integer, ForeignKey('company.id'))
+    contact_id = Column(Integer, ForeignKey('contact.id'))
+    company_id = Column(Integer, ForeignKey('company.id'))
     note = Column(VARCHAR())
     created_at = Column(DateTime, server_default=func.now())
     deleted_at = Column(DateTime)
@@ -77,13 +79,50 @@ class Phone(base):
     __tablename__ = 'phone'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    id_contact = Column(Integer, ForeignKey('contact.id'))
+    contact_id = Column(Integer, ForeignKey('contact.id'))
     label = Column(String(100))
-    phone = Column(Integer(10))
-    id_company = Column(Integer, ForeignKey('company.id'))
+    phone = Column(Integer())
+    company_id = Column(Integer, ForeignKey('company.id'))
 
     def __repr__(self):
         return '<Phone {}>'.format(self.phone)
+
+
+class Devis(base):
+    __tablename__ = 'quote'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ref_quote = Column(String)
+    quote_status = Column(ENUM('Signé', 'Envoyé', 'Retard', name='quote_status'))
+    company_id = Column(Integer, ForeignKey('company.id'))
+    created_at = Column(DateTime, server_default=func.now())
+    signed_at = Column(DateTime)
+    invoice_id = Column(Integer, ForeignKey('invoice.id'))
+    amount_id = Column(Integer, ForeignKey('amount.id'))
+
+
+class Facture(base):
+    __tablename__ = 'invoice'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ref_invoice = Column(String)
+    invoice_status = Column(ENUM('A éditer', 'Envoyé', name='invoice_status'))
+    quote_id = Column(Integer, ForeignKey('quote.id'))
+    company_id = Column(Integer, ForeignKey('company.id'))
+    created_at = Column(DateTime, server_default=func.now())
+    amount_id = Column(Integer, ForeignKey('amount.id'))
+
+
+class Amount(base):
+    __tablename__ = 'amount'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    quote_id = Column(Integer, ForeignKey('quote.id'))
+    ht_amount = Column(Numeric)
+    ttc_amount = Column(Numeric)
+    payment_status = Column(ENUM('Payé', 'En attente', 'En retard', name='payment_status'))
+    paid_at = Column(DateTime)
+    invoice_id = Column(Integer, ForeignKey('invoice.id'))
 
 
 Session = sessionmaker(db)
